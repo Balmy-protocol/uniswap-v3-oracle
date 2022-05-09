696b3e069a16361cfd85314083656ccf165464d8
Ged <0xged@protonmail.com>
2022-05-08 23:34:37 -0300

    test: all chains on mainnet

--- test/integration/fork-block-numbers.ts
+++
@@ -1,5 +0,0 @@
-const forkBlockNumber: { [name: string]: number } = {
-  ['ethereum-mainnet']: 14719370,
-};
-
-export default forkBlockNumber;
--- test/integration/static-oracle.spec.ts
+++ test/integration/static-oracle.spec.ts
@@ -10,48 +10,59 @@ import { setTestChainId } from 'utils/deploy';
 import moment from 'moment';
 import { utils } from 'ethers';
 
-const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+const PRICE_THRESHOLD = 40;
 
 contract('StaticOracle', () => {
-  let user: SignerWithAddress;
   let staticOracle: StaticOracle;
-  let snapshotId: string;
 
   const PERIOD = moment.duration('3', 'minutes').as('seconds');
 
-  before(async () => {
-    [user] = await ethers.getSigners();
-
-    await setTestChainId(1);
-
-    await evm.reset({
-      jsonRpcUrl: getNodeUrl('ethereum'),
-    });
-
-    await deployments.fixture(['StaticOracle'], { keepExistingDeployments: false });
-    staticOracle = await ethers.getContract<StaticOracle>('StaticOracle');
-
-    snapshotId = await evm.snapshot.take();
-  });
-
-  beforeEach(async () => {
-    await evm.snapshot.revert(snapshotId);
-  });
-
-  // Tests general quoting mechanism
   describe('quote', () => {
-    let feedPrice: number;
-    const PRICE_THRESHOLD = 40;
-    given(async () => {
-      feedPrice = await getLastPrice(WETH);
+    testQuoteOnNetwork({
+      network: 'ethereum',
+      chainId: 1,
+      weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+      usdc: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     });
-    it('returns correct twap', async () => {
-      const [twap] = await staticOracle.quoteAllAvailablePoolsWithTimePeriod(utils.parseEther('1'), WETH, USDC, PERIOD);
-      expect(twap).to.be.within(
-        convertPriceToNumberWithDecimals(feedPrice - PRICE_THRESHOLD, 6),
-        convertPriceToNumberWithDecimals(feedPrice + PRICE_THRESHOLD, 6)
-      );
+    testQuoteOnNetwork({
+      network: 'optimism',
+      chainId: 10,
+      weth: '0x4200000000000000000000000000000000000006',
+      usdc: '0x7f5c764cbc14f9669b88837ca1490cca17c31607',
+    });
+    testQuoteOnNetwork({
+      network: 'arbitrum',
+      chainId: 42161,
+      weth: '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+      usdc: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+    });
+    testQuoteOnNetwork({
+      network: 'polygon',
+      chainId: 137,
+      weth: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+      usdc: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
     });
   });
+
+  function testQuoteOnNetwork({ network, chainId, weth, usdc }: { network: string; chainId: number; weth: string; usdc: string }): void {
+    context(`on ${network}`, () => {
+      let feedPrice: number;
+      given(async () => {
+        await setTestChainId(chainId);
+        await evm.reset({
+          jsonRpcUrl: getNodeUrl(network),
+        });
+        await deployments.fixture(['StaticOracle'], { keepExistingDeployments: false });
+        staticOracle = await ethers.getContract<StaticOracle>('StaticOracle');
+        feedPrice = await getLastPrice(network, weth);
+      });
+      it('returns correct twap', async () => {
+        const [twap] = await staticOracle.quoteAllAvailablePoolsWithTimePeriod(utils.parseEther('1'), weth, usdc, PERIOD);
+        expect(twap).to.be.within(
+          convertPriceToNumberWithDecimals(feedPrice - PRICE_THRESHOLD, 6),
+          convertPriceToNumberWithDecimals(feedPrice + PRICE_THRESHOLD, 6)
+        );
+      });
+    });
+  }
 });
